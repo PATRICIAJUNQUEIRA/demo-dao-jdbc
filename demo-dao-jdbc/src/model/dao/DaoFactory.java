3ed4b92c59781e673489b600a5a9138e1c1d21af
Patrícia Junqueira <96187596+PATRICIAJUNQUEIRA@users.noreply.github.com>
2023-05-29 12:37:15 -0300

    Implementando a instanciação

--- demo-dao-jdbc/src/model/dao/DaoFactory.java
+++ demo-dao-jdbc/src/model/dao/DaoFactory.java
@@ -1,12 +1,14 @@
 package model.dao;
 
-import model.dao.impl.SellerDaoImplJDBC;
+import db.DB;
+import model.dao.impl.SellerDaoJDBC;
 
 public class DaoFactory {
-	
+
 	public static SellerDao createSellerDao() {
-		return new SellerDaoJDBC();	
+		return new SellerDaoJDBC(DB.getConnection());
 	}
 }
+
 
 
